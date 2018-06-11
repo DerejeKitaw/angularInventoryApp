b6a7feca6624bc0bf68c69dd34f8d42e2403d0bd
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-06-10 23:44:43 -0400

    created single component inventory app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  products: Product[];
+  product1 =  new Product(
+    'MYSHOES',
+    'Black Running Shoes',
+    '/assets/images/products/black-shoes.jpg',
+    ['Men', 'Shoes', 'Running Shoes'],
+    109.99);
+  private currentProduct: Product;
+  constructor() {
+    this.products = [
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '/assets/images/products/black-shoes.jpg',
+        ['Men', 'Shoes', 'Running Shoes'],
+        109.99),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/assets/images/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'Jackets & Vests'],
+        238.99),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/assets/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        29.99),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/assets/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        29.99),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/assets/images/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'Jackets & Vests'],
+        238.99),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/assets/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        29.99)
+      ];
+  }
+
+  productWasSelected(product: Product): void {
+    console.log('Product clicked: ', product);
+  }
+  clicked(product: Product): void {
+    this.currentProduct = product;
+    console.log(product);
+    // this.onProductSelected.emit(product);
+  }
+  isSelected(product: Product): boolean {
+    if (!product || !this.currentProduct) {
+      return false;
+    }
+    return product.sku === this.currentProduct.sku;
+  }
+//   selectedProduct(titleKey, products) {
+//     for (let i = 0; i < myArray.length; i++) {
+//       this.totalPoints = this.totalPoints + Number(myArray[i].votes) * 1;
+//         if (myArray[i].title === titleKey) {
+//             return myArray[i];
+//         }
+//     }
+// }
 }
